50393b0aaec6ca7a369b33617a5a239455030e7e
23198 <31165700+23198@users.noreply.github.com>
2020-06-26 18:34:54 +0530

    Update Appstart.sh

--- Appstart.sh
+++ Appstart.sh
@@ -7,4 +7,4 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash
 sudo apt-get install nodejs -y
 sudo npm i -g pm2
 sudo pm2 stop all
-sudo pm2 start -f ./app.js 
+sudo pm2 start -f bin/www
